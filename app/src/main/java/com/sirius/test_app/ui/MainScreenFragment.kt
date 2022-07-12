985ce4dcad984343f5d461f3461cad6065ffa6d3
seregatheone <sergey20030707@gmail.com>
2022-07-12 23:38:35 +0300

    added viewBinding

--- app/src/main/java/com/sirius/test_app/ui/MainScreenFragment.kt
+++ app/src/main/java/com/sirius/test_app/ui/MainScreenFragment.kt
@@ -6,17 +6,25 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.sirius.test_app.R
+import com.sirius.test_app.databinding.FragmentMainScreenBinding
 
 class MainScreenFragment : Fragment() {
+
+    private var _binding :FragmentMainScreenBinding? = null
+    private val binding get() = _binding!!
+
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-
-        return inflater.inflate(R.layout.fragment_main_screen, container, false)
-    }
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    ): View{
+        _binding = FragmentMainScreenBinding.inflate(layoutInflater,container,false)
+        return binding.root
     }
 
+
+    override fun onDestroy() {
+        _binding = null
+        super.onDestroy()
+    }
 }
